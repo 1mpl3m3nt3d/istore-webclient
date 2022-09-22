ca3ad100792f44f350fa88536c9354ef3f525f90
1mpl3m3nt3d <105541001+1mpl3m3nt3d@users.noreply.github.com>
2022-09-22 21:27:09 +0300

    Up

--- src/utils/oidcConfig.ts
+++ src/utils/oidcConfig.ts
@@ -1,6 +1,7 @@
 import { UserManagerSettings, WebStorageStateStore } from 'oidc-client-ts';
 
 export const oidcConfig: UserManagerSettings = {
+  accessTokenExpiringNotificationTimeInSeconds: 60,
   authority: `${process.env.REACT_APP_IDENTITY_URL}`,
   automaticSilentRenew: true,
   client_authentication: 'client_secret_post',
@@ -9,13 +10,23 @@ export const oidcConfig: UserManagerSettings = {
   filterProtocolClaims: true,
   includeIdTokenInSilentRenew: true,
   loadUserInfo: true,
+  mergeClaims: false,
+  monitorAnonymousSession: true,
+  monitorSession: true,
   post_logout_redirect_uri: `${process.env.REACT_APP_POST_LOGOUT_REDIRECT_URL}`,
   prompt: 'login',
   redirect_uri: `${process.env.REACT_APP_REDIRECT_URL}`,
+  redirectMethod: 'assign',
+  refreshTokenCredentials: 'same-origin',
+  response_mode: 'query',
   response_type: `${process.env.REACT_APP_RESPONSE_TYPE}`,
+  revokeTokensOnSignout: true,
   scope: `${process.env.REACT_APP_SCOPE}`,
   silent_redirect_uri: `${process.env.REACT_APP_SILENT_REDIRECT_URL}`,
   silentRequestTimeoutInSeconds: 15,
+  staleStateAgeInSeconds: 300,
+  stateStore: new WebStorageStateStore({ store: window.sessionStorage }),
+  stopCheckSessionOnError: true,
   userStore: new WebStorageStateStore({ store: window.sessionStorage }),
   validateSubOnSilentRenew: true,
 };
